1a92b9965891faf0a3fee5db38cdce49362c9527
Treasure-Praise <mac@Treasure-Praises-MacBook-Pro.local>
2022-10-12 12:24:20 +0100

    modified header component

--- src/componets/layout/Header.jsx
+++ src/componets/layout/Header.jsx
@@ -5,15 +5,18 @@ import { Avatar } from "@material-tailwind/react"
 export default function Header() {
   return (
     <div className='flex justify-between items-center   '>
-      {/* <Link to="/about" element={<About/>}>
-            About
-        </Link> */}
-      <Avatar
-        src='https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/v1499051169/xrir1aymhq9clo96uhbn.png'
-        size='xl'
-        alt='avatar'
-        variant='circular'
-      />
+      <a
+        href='https://www.thriveagric.com/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <Avatar
+          src='https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/v1499051169/xrir1aymhq9clo96uhbn.png'
+          size='xl'
+          alt='avatar'
+          variant='circular'
+        />
+      </a>
       <FaGithub size={30} />
     </div>
   )
