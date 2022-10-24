4243fdd45bf1c80f7d86c44ed07c69e262b61d97
Treasure-Praise <mac@Treasure-Praises-MacBook-Pro.local>
2022-10-24 12:49:22 +0100

    updates to package.json

--- src/pages/UserDisplay.stories.jsx
+++ src/pages/UserDisplay.stories.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
 import Header from "../componets/layout/Header"
 import Footer from "../componets/layout/Footer"
-import UserSearch from "../componets/layout/user/UserSearch"
 import { UsersContextProvider } from "../context/UsersContext"
 import UserDisplay from "./UserDisplay"
 import UserSkeleton from "../componets/UserSkeleton"
@@ -17,7 +16,6 @@ export const userDisplay = () => {
       <UsersContextProvider>
         <BrowserRouter>
           <Header />
-          <UserSearch />
           <UserSkeleton />
           <Footer />
         </BrowserRouter>
